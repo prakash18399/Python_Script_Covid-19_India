240bc481130dd73458beb7b7c028782afe621d60
prakash18399 <prakashchoudhary1831999@gmail.com>
2020-09-10 19:31:03 +0530

    added pie chart

--- pyCovid.py
+++ pyCovid.py
@@ -1,4 +1,6 @@
+import socket
 import requests
+import matplotlib.pyplot as plt
 
 data = requests.get('https://api.rootnet.in/covid19-in/stats/latest').json()
 length1 = len(data['data']['unofficial-summary'])
@@ -28,4 +30,36 @@ for j in range(length2):
 	print("*****************************************************")
 	print("\n\n")
 
+
+num = []
+cases = ['total','recovered','deaths','active']
+
+try:
+		socket.create_connection(("www.google.com",80))
+		api = "https://api.covidindiatracker.com/total.json"
+		res = requests.get(api)
+		json = res.json()
+
+		t = json['confirmed']
+		r = json['recovered']
+		d = json['deaths']
+		a = json['active']
+		
+		num.append(t)
+		
+		num.append(r)
+	
+		num.append(d)
+
+		num.append(a)
+
+		print(num)
+
+except OSError as e:
+		print(e)
+
+
+plt.pie(num, labels = cases , explode = [0,0.05,0.2,0.1] , shadow = True , startangle = 45 , colors = ['lightskyblue','g','r','orange'] , autopct = "%0.2f%%")
+plt.title("Covid-19 Cases", fontsize = 18)
+plt.show()
 
